123ff0e616095aef1aec6499b1e8d22dfb89756f
김경록 <goodboy3421@gmail.com>
2020-01-20 12:09:55 +0900

    refactor: 예외처리, 하드코딩 변수
    carName, tryNumber의 입력에 관련된 예외처리 케이스 추가, 하드코딩 개선(",")

--- src/main/java/utils/InputUtils.java
+++ src/main/java/utils/InputUtils.java
@@ -1,24 +1,33 @@
 package utils;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputUtils {
-    private static final Scanner INPUT = new Scanner(System.in);
+    private static Scanner input;
     private static final int MAX_NAME_LENGTH = 5;
+    private static final String COMMA = ",";
 
     public static List<String> inputCarNames() {
         while (true) {
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-            String input = INPUT.nextLine();
-            List<String> list = Arrays.asList(input.split(","));
-            if (list.size() > 0 && verifyCarName(list)) {
+            try {
+                input = new Scanner(System.in);
+                String input = InputUtils.input.nextLine();
+                List<String> list = Arrays.asList(input.split(COMMA));
+
+                if (!verifyCarName(list)) {
+                    throw new InputCarNameException("이름은 1자이상 5자 이하만 가능합니다.");
+                }
+                if (list.size() <= 1) {
+                    throw new InputCarNameException("최소 2대의 차량이 있어야합니다.");
+                }
                 return list;
+            } catch (InputCarNameException e) {
+                // 예외처리를 이렇게 진행하는게 좋은걸까? inputTryNumber의 커스텀 예외처리 방식처럼 쓰는게 좋은걸까?
             }
-            System.out.println("입력 형식이 옳지 않습니다.");
         }
     }
 
@@ -35,14 +44,19 @@ public class InputUtils {
         while (true) {
             System.out.println("시도할 횟수는 몇회인가요?");
             try {
-                int tryNumber = INPUT.nextInt();
-                if (tryNumber > 0) {
-                    return tryNumber;
+                input = new Scanner(System.in);
+                int tryNumber = input.nextInt();
+
+                if (tryNumber <= 0) {
+                    throw new InputTryNumberException();
                 }
+                return tryNumber;
+
             } catch (InputMismatchException e) {
                 System.out.println("숫자를 입력해주세요!");
+            } catch (InputTryNumberException e) {
+                System.out.println("0이상의 값을 입력해주세요!");
             }
         }
     }
-
 }
