40d75e6512d767cb130868613c4705bb562089de
김경록 <goodboy3421@gmail.com>
2020-01-20 10:58:14 +0900

    feat: Main 기능
    자동차경주게임을 시작하는 메인 클래스

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import domain.RacingGameSystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RacingGameSystem racingGameSystem = new RacingGameSystem();
+        racingGameSystem.play();
+    }
+}
