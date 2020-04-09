b2e399ea47241e18364f36bff043603e3e6f6715
김경록 <goodboy3421@gmail.com>
2020-04-09 22:06:11 +0900

    refactor: Car model 리팩토링
    기본자료형을 랩핑처리 함

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,23 +1,48 @@
 package domain;
 
+import java.util.Objects;
+
 public class Car {
-    private final String name;
+    private final int STEP_FORWARD_STANDARD = 4;
+
+    private final Name carName;
     private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
+    public Car(String carName) {
+        this.carName = new Name(carName);
     }
 
-    public void moveOneStep() {
-        position++;
+    public void goOrStop(int random) {
+        if (random >= STEP_FORWARD_STANDARD) {
+            position++;
+        }
     }
 
     public int getPosition() {
         return position;
     }
 
-    public String getName() {
-        return name;
+    public Name getCarName() {
+        return carName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Car car = (Car) obj;
+        return Objects.equals(carName, car.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName);
     }
 }
 
